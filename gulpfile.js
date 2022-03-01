7fc1d13b584dadc09d2eaa9161ef184db5c0278d
Ke Jin <ke.jin@laserfiche.com>
2022-03-01 10:01:26 -0800

    use declaration file

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const replace = require('gulp-replace');
 
 const INDEX_JS_PATH = './dist/index.js';
 const TSLIB = 'tslib';
-const TSLIBCDN = 'https://cdn.jsdelivr.net/npm/tslib@2/tslib.min.js';
+const TSLIBCDN = 'https://cdn.jsdelivr.net/npm/tslib@2.3.1/tslib.d.ts';
 
 async function replaceImportsInIndex(){
     src(INDEX_JS_PATH, {base: './'})
